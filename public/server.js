56d7a1773658143788295bee97cfa51a4d18adfa
lalmonkey <117243861+lalmonkey@users.noreply.github.com>
2023-12-03 21:00:01 +0900

    Update server.js

--- public/server.js
+++ public/server.js
@@ -7,7 +7,7 @@ const mysql = require('mysql2');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+
 app.use(bodyParser.json()); 
 
 const connection = mysql.createConnection({
